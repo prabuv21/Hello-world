daa53ddeb9216ac601631746dba7e4d438c28413
prabuv21 <prabuv.88@gmail.com>
2017-03-25 14:04:31 +0530

    Update HelloWorldPrabu.java

--- HelloWorldPrabu.java
+++ HelloWorldPrabu.java
@@ -1,6 +1,6 @@
 public class HelloWorldPrabu{
 
 	public static void main(String [] args) {
-       System.out.println("Hello Prabu on the mission right now");
+       System.out.println("Hello all");
     }
 }
