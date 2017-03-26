7997626206d7e15f3753a34906d9269a9eeaa326
prabuv21 <prabuv.88@gmail.com>
2017-03-26 12:52:58 +0530

    Update HelloWorldPrabu.java

--- HelloWorldPrabu.java
+++ HelloWorldPrabu.java
@@ -1,6 +1,6 @@
 public class HelloWorldPrabu{
 
 	public static void main(String [] args) {
-       System.out.println("Hello all");
+       System.out.println("Welcome to AWS cloud Prabu");
     }
 }
