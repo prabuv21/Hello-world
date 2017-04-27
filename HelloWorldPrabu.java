dad217db594de721bb0ae2ee4470fdb630bf15fb
prabuv21 <prabuv.88@gmail.com>
2017-04-27 20:06:25 +0530

    Update HelloWorldPrabu.java

--- HelloWorldPrabu.java
+++ HelloWorldPrabu.java
@@ -1,6 +1,6 @@
 public class HelloWorldPrabu{
 
 	public static void main(String [] args) {
-       System.out.println("Welcome to AWS cloud V Prabu");
+       System.out.println("Welcome to AWS cloud Venkatapathy Prabu");
     }
 }
